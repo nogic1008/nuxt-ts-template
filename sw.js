e50121e7a895201abd8702140f213e4ca427c021
nogic1008 <nogic1008@users.noreply.github.com>
2020-02-04 00:22:03 +0000

    ci: auto deployment from 1a2352e905a320c82e1903135362ff33fb673178

--- sw.js
+++ sw.js
@@ -28,5 +28,5 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('nuxt-ts-template/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
-workbox.routing.registerRoute(new RegExp('nuxt-ts-template/'), new workbox.strategies.NetworkFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/nuxt-ts-template/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/nuxt-ts-template/'), new workbox.strategies.NetworkFirst ({}), 'GET')
