1c3dcfd7d147eec7608309f4b562f5215b859b6f
nogic1008 <24802730+nogic1008@users.noreply.github.com>
2020-03-11 12:00:48 +0900

    feat: support i18n on all pages

--- __tests__/pages/index.test.ts
+++ __tests__/pages/index.test.ts
@@ -1,10 +1,12 @@
 import { createLocalVue, mount, shallowMount, Wrapper } from '@vue/test-utils'
 import Buefy from 'buefy'
+import VueI18n from 'vue-i18n'
 
 import Index from '~/pages/index.vue'
 
 const localVue = createLocalVue()
 localVue.use(Buefy)
+localVue.use(VueI18n)
 
 describe('pages/index.vue', () => {
   let wrapper: Wrapper<Vue>
@@ -12,16 +14,28 @@ describe('pages/index.vue', () => {
     counter: { count: 0 }
   }
 
-  beforeEach(() => {
+  beforeEach(async () => {
     wrapper = shallowMount(Index, { localVue, mocks: { $vxm } })
+    wrapper.vm.$i18n.locale = 'en'
+    await localVue.nextTick()
   })
 
   test('is a Vue instance', () => {
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 
-  test('renders correctly', () => {
-    const wrapper = mount(Index, { localVue, mocks: { $vxm } })
-    expect(wrapper.element).toMatchSnapshot()
+  describe('snapshot', () => {
+    test.each(['en', 'ja'])(
+      'renders correctly if locale is "%s"',
+      async (locale) => {
+        const wrapper = mount(Index, {
+          localVue,
+          mocks: { $vxm }
+        })
+        wrapper.vm.$i18n.locale = locale
+        await localVue.nextTick()
+        expect(wrapper.element).toMatchSnapshot()
+      }
+    )
   })
 })
--- __tests__/pages/inspire.test.ts
+++ __tests__/pages/inspire.test.ts
@@ -1,10 +1,12 @@
 import { createLocalVue, mount, shallowMount, Wrapper } from '@vue/test-utils'
 import Buefy from 'buefy'
+import VueI18n from 'vue-i18n'
 
 import Inspire from '~/pages/inspire.vue'
 
 const localVue = createLocalVue()
 localVue.use(Buefy)
+localVue.use(VueI18n)
 
 describe('pages/inspire.vue', () => {
   let wrapper: Wrapper<Vue>
@@ -17,8 +19,15 @@ describe('pages/inspire.vue', () => {
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 
-  test('renders correctly', () => {
-    const wrapper = mount(Inspire, { localVue })
-    expect(wrapper.element).toMatchSnapshot()
+  describe('snapshot', () => {
+    test.each(['en', 'ja'])(
+      'renders correctly if locale is "%s"',
+      async (locale) => {
+        const wrapper = mount(Inspire, { localVue })
+        wrapper.vm.$i18n.locale = locale
+        await localVue.nextTick()
+        expect(wrapper.element).toMatchSnapshot()
+      }
+    )
   })
 })
