14f5576da687395854a57ce746e52ad60246dcef
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-04-19 13:57:38 +0900

    feat: remove store components

--- store/index.ts
+++
@@ -1,8 +0,0 @@
-/* istanbul ignore file */
-import { getAccessorType } from 'typed-vuex'
-
-import * as counter from '~/store/counter'
-
-export const accessorType = getAccessorType({
-  modules: { counter }
-})
